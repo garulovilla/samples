e95ac7fc64ab6b7f49ee67b097efbb3943c5830c
Garu Lovilla <garu.lovilla@gmail.com>
2019-07-30 18:32:19 +0000

    Product selection exercise - Copy of the previous product index exercise panel. - Replace Input control with a Select control. To fill the Select control, the concept of aggregation was used. - Add logic to know which item was selected for display the product details.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -130,6 +130,27 @@ sap.ui.define([
 			sMessage = this._getTextResourceBundle("messageDisplayingDetails", [oProduct.ProductId, sIndex]);
 			MessageToast.show(sMessage);
 		},
+		
+		onProductSelectionChange: function (oEvent) {
+			var oSelectedProduct = oEvent.getParameter("selectedItem"),
+				oContext = oSelectedProduct.getBindingContext("products"),
+				sPath = oContext.getPath(),
+				oProductObject = this.getView().getModel("products").getObject(sPath),
+				oProductSelected = this.byId("productSelected"),
+				oProductSelectedDetailsContainer = this.byId("productSelectedDetailsContainer");
+
+			// Set product selected data in TextArea
+			oProductSelected.setValue(JSON.stringify(oProductObject, null, "\t"));
+
+			// Set new binding context [sap.ui.base.ManagedObject]
+			oProductSelectedDetailsContainer.setBindingContext(oContext, "products");
+
+			// Set binding element [sap.ui.core.Element]
+			// oProductSelectedDetailsContainer.bindElement({
+			// 	path: sPath,
+			// 	model: "products"
+			// });
+		},
         
         // Private
 		// --------------------------------------------------------------------------------
