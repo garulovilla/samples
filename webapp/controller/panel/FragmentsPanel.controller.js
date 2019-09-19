37be24dcdeb7b71d6313eac363bfa2e2485dfa7b
Garu Lovilla <garu.lovilla@gmail.com>
2019-09-19 21:58:26 +0000

    Logging in fragment controller - Add a prototype call to lifecycle hooks.

--- webapp/controller/panel/FragmentsPanel.controller.js
+++ webapp/controller/panel/FragmentsPanel.controller.js
@@ -11,6 +11,8 @@ sap.ui.define([
 		// ------------------------------------------------------------------------------------------------------------------------
 		
 		onInit: function() {
+			BaseController.prototype.onInit.apply(this, arguments);
+			
 			// Set new Text control with local rocket id
 			this.byId("localContainer").addItem(
 				new sap.m.Text({ text: `#${this.byId("localRocket").getId()}` })
