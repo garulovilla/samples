f7727fb2c8b2a192f2f6582465fe68501633f463
Garu Lovilla <garu.lovilla@gmail.com>
2019-09-19 21:50:52 +0000

    Table with category filter - Add new fragment with an input for category filter and Table control. - Add new fragment for category filter with a SelectDialog control. - Add products stats model for category filter in manifest.json.

--- webapp/controller/panel/FragmentsPanel.controller.js
+++ webapp/controller/panel/FragmentsPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"com/chlv/samples/controller/BaseController",
-	"sap/ui/core/Fragment"
-], function (BaseController, Fragment) {
+	"sap/ui/core/Fragment",
+	"sap/ui/model/Filter"
+], function (BaseController, Fragment, Filter) {
 	"use strict";
 
 	return BaseController.extend("com.chlv.samples.controller.panel.FragmentsPanel", {
@@ -74,6 +75,63 @@ sap.ui.define([
 
 		onCloseHelloDialog: function () {
 			this.byId("helloDialog").close();
+		},
+		
+		onValueHelpRequest: function() {
+			var oView = this.getView(),
+				oCategoryFilterDialog = this.byId(Fragment.createId("productsTableWithCategoryFilter", "categoryFilterDialog"));
+				
+			if (!oCategoryFilterDialog) {
+				Fragment.load({
+					id: this.createId("productsTableWithCategoryFilter"),
+					name: "com.chlv.samples.view.fragment.CategoryFilterSelectDialog",
+					controller: this
+				}).then(function (oDialog) {
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				oCategoryFilterDialog.open();
+			}
+		},
+		
+		handleSearch: function(oEvent) {
+			var sValue = oEvent.getParameter("value");
+			var oFilter = new Filter("text", sap.ui.model.FilterOperator.Contains, sValue);
+			var oBinding = oEvent.getSource().getBinding("items");
+			oBinding.filter([oFilter]);
+		},
+		
+		handleClose: function(oEvent) {
+			var oSelectedItem = oEvent.getParameter("selectedItem"),
+				oInput = this.byId(Fragment.createId("productsTableWithCategoryFilter", "categoryFilterInput"));
+			
+			if (oSelectedItem) {
+				oInput.setValue(oSelectedItem.getTitle());
+			}
+
+			if (!oSelectedItem) {
+				oInput.resetProperty("value");
+			}
+			
+			this._updateFilters(oInput.getValue());
+		},
+		
+		// Private
+		// ------------------------------------------------------------------------------------------------------------------------
+		
+		_updateFilters: function(sCategory) {
+			var aFilters = [];
+			
+			if (sCategory && sCategory.length > 0) {
+				var filter = new Filter("Category", sap.ui.model.FilterOperator.EQ, sCategory);
+				aFilters.push(filter);
+			}
+
+			// Update select binding
+			var oSelect = this.byId(Fragment.createId("productsTableWithCategoryFilter", "products"));
+			var oItemAggregation = oSelect.getBinding("items");
+			oItemAggregation.filter(aFilters, "Application");
 		}
 	});
 
