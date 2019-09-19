f34be1d6de02649ed0d78b8a0ac268a05f1b5f5b
Garu Lovilla <garu.lovilla@gmail.com>
2019-09-19 20:37:55 +0000

    Programmatically instantiating fragments - Add view for fragments exercises. - Add custom style for VBox containers. - Add Rocket fragment. - Add rocket image.

---
+++ webapp/controller/panel/FragmentsPanel.controller.js
@@ -0,0 +1,53 @@
+sap.ui.define([
+	"com/chlv/samples/controller/BaseController",
+	"sap/ui/core/Fragment"
+], function (BaseController, Fragment) {
+	"use strict";
+
+	return BaseController.extend("com.chlv.samples.controller.panel.FragmentsPanel", {
+		
+		// Lifecycle Hooks
+		// ------------------------------------------------------------------------------------------------------------------------
+		
+		onInit: function() {
+			this.byId("localContainer").addItem(
+				new sap.m.Text({ text: `#${this.byId("localRocket").getId()}` })
+			);
+		},
+		
+		// Handlers
+		// ------------------------------------------------------------------------------------------------------------------------
+		
+		onFragmentInstantiation: function(iMode) {
+    		var oContainerInstantiation = this.byId("programmaticallyContainer" + iMode);
+			var sId = "";
+			
+			if(!oContainerInstantiation) {
+				return;
+			}
+			
+			switch(iMode) {
+				case 0:
+					break;
+				case 1:
+					sId = "myFragment";
+					break;
+				case 2:
+					sId = this.getView().getId();
+					break;
+				case 3:
+					sId = this.createId("myFragment");
+					break;
+			}
+			
+			Fragment.load({
+				name: "com.chlv.samples.view.fragment.Rocket",
+				id: sId
+			}).then(function(oContent) {
+				oContainerInstantiation.addItem(oContent);
+				oContainerInstantiation.addItem(new sap.m.Text({ text: `#${oContent.getId()}` }));
+			});
+		}
+	});
+
+});
